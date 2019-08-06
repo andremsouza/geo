2be2b85901aaee06adf0a69212030235acee1fc9
André Moreira Souza <msouza.andre@hotmail.com>
2019-08-06 17:50:04 -0300

    Using passlib for password hashing.

--- api/config.py
+++ api/config.py
@@ -1,4 +1,11 @@
+from passlib.context import CryptContext
+
+MIN_CONNECTIONS = 1
+MAX_CONNECTIONS = 100
 HOST = 'localhost'
+PORT = 5432
 DBNAME = 'icgeo'
 USER = 'miner'
 PASSWORD = 'minerpass'
+
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
